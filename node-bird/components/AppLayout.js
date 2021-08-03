70262ed6b95b83dc81b728ee5a1bc18a6dbe17fd
bweddi <bweddi.k@gmail.com>
2021-08-03 23:29:56 +0900

    init NodeBird 클론코딩 ~섹션1 반응형 그리드 사용하기

---
+++ node-bird/components/AppLayout.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Menu, Input, Row, Col } from 'antd';
+
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          하나
+        </Col>
+        <Col xs={24} md={12}>
+          둘
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://github.com/bweddi"
+            target="_blank"
+            rel="n:wqoreferrer noopener noreferrer"
+          >
+            bewddi
+          </a>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
---
+++ node-bird/pages/signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+import Head from 'next/head';
+
+const Signup = () => {
+  return (
+    <>
+      <Head>
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      <AppLayout>Signup~</AppLayout>
+    </>
+  );
+};
+
+export default Signup;
